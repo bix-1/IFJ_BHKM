722361780bf0a12d1e255283e53100f51841b4b5
Tomáš Hladký <tofisko@gmail.com>
2020-10-18 23:27:03 +0200

    Added codegen implementation files

---
+++ codegen.c
@@ -0,0 +1,10 @@
+/*
+ * Purpose: Compiler implementation of imperative programing language IFJ20
+ *
+ * Authors: Hladký Tomáš	xhladk15@stud.fit.vutbr.cz
+ *          Kostolányi Adam	xkosto04@stud.fit.vutbr.cz
+ *          Makiš Jozef	    xmakis00@stud.fit.vutbr.cz
+ *          Bartko Jakub	xbartk07@stud.fit.vutbr.cz
+ */
+
+#include "codegen.h"
