65c5c4d9925edfd2cdd341e7c8bf1b3b6a327794
Jozef Makis <xmakis00@stud.fit.vutbr.cz>
2020-10-19 22:14:20 +0200

    Edited identificators Bug fix Added comments

--- str.c
+++ str.c
@@ -1,10 +1,13 @@
 /*
- * Purpose: Compiler implementation of imperative programing language IFJ20
+ * Project: Compiler for imperative programing language IFJ20
  *
- * Authors: Hladký Tomáš	xhladk15@stud.fit.vutbr.cz
- *          Kostolányi Adam	xkosto04@stud.fit.vutbr.cz
- *          Makiš Jozef	    xmakis00@stud.fit.vutbr.cz
- *          Bartko Jakub	xbartk07@stud.fit.vutbr.cz
+ * File: str.c
+ * Brief: Implementation of dynamic string.
+ *
+ * Authors: Hladký Tomáš    xhladk15@stud.fit.vutbr.cz
+ *          Kostolányi Adam xkosto04@stud.fit.vutbr.cz
+ *          Makiš Jozef     xmakis00@stud.fit.vutbr.cz
+ *          Bartko Jakub    xbartk07@stud.fit.vutbr.cz
  */
 
 #include "str.h"
@@ -12,9 +15,10 @@
 #include "string.h"
 
 /*
- * Function creates new string
+ * Function creates new string.
  */
 int str_init(string *s){
+    //allocates string to 1 char size
     s->str = (char *) malloc(STR_LEN_INC);
 
     if(s->str == NULL){
@@ -23,7 +27,7 @@ int str_init(string *s){
 
     s->str[0] = '\0';
     s->str_lenght = 0;
-    s->alloc_size = 0;
+    s->str_alloc_size = 0;
 }
 
 /*
@@ -34,7 +38,7 @@ void str_free(string *s){
 }
 
 /*
- * Function clears string array
+ * Function clears string array.
  */
 void str_clear(string *s){
     s->str[0] = '\0';
@@ -42,56 +46,58 @@ void str_clear(string *s){
 }
 
 /*
- * Function adds new char intostring array
+ * Function adds new char intostring array.
  */
-int str_add_char(string *s1, char c){
-    if(s1->str_lenght + 1 >= s1->alloc_size){
+int str_add_char(string *s, char c){
+    if(s->str_lenght + 1 >= s->str_alloc_size){
         //if there is not enoguh memory, we reallocate
-        s1->str = (char *) realloc(s1->str, s1->str_lenght + STR_LEN_INC);
+        s->str = (char *) realloc(s->str, s->str_lenght + STR_LEN_INC);
 
-        if(s1->str == NULL){
+        if(s->str == NULL){
             return 1;
         }
 
-        s1->alloc_size = s1->str_lenght + STR_LEN_INC;
+        s->str_alloc_size = s->str_lenght + STR_LEN_INC;
     }
     //adds char and increments lenght for '\0'
-    s1->str[s1->str_lenght] = c;
-    s1->str_lenght++;
-    s1->str[s1->str_lenght] = '\0';
+    s->str[s->str_lenght] = c;
+    s->str_lenght++;
+    s->str[s->str_lenght] = '\0';
+
     return 0;
 }
 
 /*
- * Function copies second string into the first
+ * Function copies second string into the first.
  */
 int str_copy(string *s1, string *s2){
     int new_str_lenght = s2->str_lenght;
     //if there is not enoguh memory, we reallocate
-    if(new_str_lenght >= s1->alloc_size){
-        s1->str = (char *) realloc(s1->str, new_str_lenght + 11);
+    if(new_str_lenght >= s1->str_alloc_size){
+        s1->str = (char *) realloc(s1->str, new_str_lenght + 1);
 
         if(s1->str == NULL){
             return 1;
         }
 
-        s1->alloc_size = new_str_lenght + 1;
+        s1->str_alloc_size = new_str_lenght + 1;
     }
     //copies string and and changes new lenght
     strcpy(s1->str, s2->str);
     s1->str_lenght = new_str_lenght;
+
     return 0;
 }
 
 /*
- * Function compares string with constant
+ * Function compares string with constant.
  */
-int str_cmp_cons(string *s1, char *s2){
-    return strcmp(s1->str, s2);
+int str_cmp_cons(string *s, char *s_const){
+    return strcmp(s->str, s_const);
 }
 
 /*
- * Function comapares two strings
+ * Function comapares two strings.
  */
 int str_cmp(string *s1, string *s2){
     return strcmp(s1->str, s2->str);
