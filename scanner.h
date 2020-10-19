89587c9fa1e664500b446e4088f0a70f0e723ef9
Jozef Makis <xmakis00@stud.fit.vutbr.cz>
2020-10-19 19:13:42 +0200

    Scanner header file init

---
+++ scanner.h
@@ -0,0 +1,13 @@
+/*
+ * Purpose: Compiler implementation of imperative programing language IFJ20
+ *
+ * Authors: Hladký Tomáš	xhladk15@stud.fit.vutbr.cz
+ *          Kostolányi Adam	xkosto04@stud.fit.vutbr.cz
+ *          Makiš Jozef	    xmakis00@stud.fit.vutbr.cz
+ *          Bartko Jakub	xbartk07@stud.fit.vutbr.cz
+ */
+
+#ifndef IFJ_BHKM_SCANNER_H
+#define IFJ_BHKM_SCANNER_H
+
+#endif //IFJ_BHKM_SCANNER_H
