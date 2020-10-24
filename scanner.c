61b28a024f583dab2745ed7f70cedbca198de939
Jozef Makis <xmakis00@stud.fit.vutbr.cz>
2020-10-24 21:21:23 +0200

    Added scanner state for integer literal Added scanner state for decimal and exponent literals Added attribute values for keywords and identifier

--- scanner.c
+++ scanner.c
@@ -19,7 +19,12 @@ void source_file_setup(FILE *f) {
 int get_next_token(string *attr) {
     int scanner_state = s_start; // we set initial scanner state
     int c;
+    char *endptr; // auxiliary variable for strtod function
     Token token;
+
+    if (str_init(token.attr.str_lit) == 1) {
+        return I_ERROR;
+    }
 
     str_clear(attr); // deleting everything in string, if it is identifier we start storing data
 
@@ -37,6 +42,9 @@ int get_next_token(string *attr) {
                 } else if (isdigit(c)) {
                     str_add_char(attr, c); //save first char
                     scanner_state = s_int_lit;
+                } else if (c == '"') {
+                    str_add_char(attr, c); //save first char
+                    scanner_state = s_string_tmp;
                 } else if (c == '/') {
                     c = getc(source);
                     if (c == '/') {
@@ -81,8 +89,8 @@ int get_next_token(string *attr) {
                         str_free(attr);
                         return L_SUCCESS;
                     } else {
-                        token.token_type = T_ASSIGNMENT;
                         ungetc(c, source);
+                        token.token_type = T_ASSIGNMENT;
                         str_free(attr);
                         return L_SUCCESS;
                     }
@@ -134,36 +142,128 @@ int get_next_token(string *attr) {
                     return L_ERROR;
                 }
                 break;
-            case s_identifier:
+            case s_identifier: //f15
+                // we are filling our string with indentifier
                 if (isalnum(c) || c == '_') {
                     str_add_char(attr, c);
                 } else {
+                    if (str_init(token.attr.str_lit) == 1) {
+                        return I_ERROR;
+                    }
+
+                    ungetc(c, source); // we read all and we need to go one char back
+
+                    // we check for keyword, if there is one we do not treat him as identifier, and
+                    // we store his token and his string attribute
                     if (str_cmp_cons(attr, "else") == 0) {
                         token.token_type = T_ELSE;
+                        str_copy(token.attr.str_lit, attr);
                     } else if (str_cmp_cons(attr, "float64") == 0) {
                         token.token_type = T_FLOAT64;
+                        str_copy(token.attr.str_lit, attr);
                     } else if (str_cmp_cons(attr, "for") == 0) {
                         token.token_type = T_FOR;
+                        str_copy(token.attr.str_lit, attr);
                     } else if (str_cmp_cons(attr, "func") == 0) {
                         token.token_type = T_FUNC;
+                        str_copy(token.attr.str_lit, attr);
                     } else if (str_cmp_cons(attr, "if") == 0) {
                         token.token_type = T_IF;
+                        str_copy(token.attr.str_lit, attr);
                     } else if (str_cmp_cons(attr, "int") == 0) {
                         token.token_type = T_INT;
+                        str_copy(token.attr.str_lit, attr);
                     } else if (str_cmp_cons(attr, "package") == 0) {
                         token.token_type = T_PACKAGE;
+                        str_copy(token.attr.str_lit, attr);
                     } else if (str_cmp_cons(attr, "return") == 0) {
                         token.token_type = T_RETRUN;
+                        str_copy(token.attr.str_lit, attr);
                     } else if (str_cmp_cons(attr, "string") == 0) {
                         token.token_type = T_STRING;
+                        str_copy(token.attr.str_lit, attr);
                     } else {
                         token.token_type = T_IDENTIFIER;
+                        str_copy(token.attr.str_lit, attr);
                     }
+
                     str_free(attr);
                     return L_SUCCESS;
                 }
                 break;
-            case s_int_lit:
+            case s_int_lit: //f16
+                if (isdigit(c)) {
+                    str_add_char(attr, c);
+                } else if (c == 'e' || c == 'E') {
+                    str_add_char(attr, c);
+                    scanner_state = s_exp_tmp;
+                } else if (c == '.') {
+                    str_add_char(attr, c);
+                    scanner_state = s_decimal_tmp;
+                } else {
+                    ungetc(c, source);
+                    token.token_type = T_INT;
+                    token.attr.int_lit = atoi(attr->str); // we dont need to check for conversion errors,
+                    //because we know that just integer number literal
+                }
+                break;
+            case s_exp_tmp: //q18
+                if(isdigit(c)){
+                    str_add_char(attr, c);
+                    scanner_state = s_decimal_lit;
+                } else if (c == '+' || c == '-'){
+                    scanner_state = s_exp_sig_tmp;
+                } else {
+                    ungetc(c, source);
+                    str_free(attr);
+                    return L_ERROR;
+                }
+                break;
+            case s_exp_sig_tmp: //r18
+                if(isdigit(c)){
+                    str_add_char(attr, c);
+                    scanner_state = s_exp_lit;
+                } else {
+                    ungetc(c, source);
+                    str_free(attr);
+                    return L_ERROR;
+                }
+                break;
+            case s_decimal_tmp: //q17
+                if(isdigit(c)){
+                    str_add_char(attr, c);
+                    scanner_state = s_decimal_lit;
+                } else {
+                    ungetc(c, source);
+                    str_free(attr);
+                    return L_ERROR;
+                }
+                break;
+            case s_decimal_lit: //f17
+                if (isdigit(c)) {
+                    str_add_char(attr, c);
+                } else if (c == 'e' || c == 'E') {
+                    str_add_char(attr, c);
+                    scanner_state = s_exp_tmp;
+                } else {
+                    ungetc(c, source);
+                    token.token_type = T_FLOAT64;
+                    token.attr.dec_lit = strtod(attr->str, &endptr);
+                }
+                break;
+            case s_exp_lit: //f18
+                if(isdigit(c)){
+                    str_add_char(attr, c);
+                } else if (c == '+' || c == '-'){
+                    str_add_char(attr, c);
+                    scanner_state = s_decimal_lit;
+                } else {
+                    ungetc(c, source);
+                    token.token_type = T_FLOAT64;
+                    token.attr.dec_lit = strtod(attr->str, &endptr);
+                }
+                break;
+            case s_string_tmp:
                 //TODO
                 break;
             case s_line_c:
