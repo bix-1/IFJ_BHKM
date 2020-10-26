7b543bf46bc6fd583779284277e375e67611aebc
Jozef Makis <xmakis00@stud.fit.vutbr.cz>
2020-10-26 18:38:32 +0100

    Function comments updated

--- str.h
+++ str.h
@@ -21,18 +21,39 @@ typedef struct {
     int str_alloc_size; // size of allocated memory
 } string;
 
+/*
+ * Function creates new string.
+ */
 int str_init(string *s);
 
+/*
+ * Function frees allocated memory
+ */
 void str_free(string *s);
 
+/*
+ * Function clears string array.
+ */
 void str_clear(string *s);
 
+/*
+ * Function adds new char intostring array.
+ */
 int str_add_char(string *s, char c);
 
+/*
+ * Function copies second string into the first.
+ */
 int str_copy(string *s1, string *s2);
 
+/*
+ * Function compares string with constant.
+ */
 int str_cmp_cons(string *s, char *s_const);
 
+/*
+ * Function comapares two strings.
+ */
 int str_cmp(string *s1, string *s2);
 
 #endif //IFJ_BHKM_STR_H
