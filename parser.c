76775f45f645570cf193b41c8dbb0f7b4bf1d8e3
xbartk07 <xbartk07@stud.fit.vutbr.cz>
2020-10-26 19:08:43 +0100

    Added parser files

---
+++ parser.c
@@ -0,0 +1,15 @@
+/*
+ * Project: Compiler for imperative programing language IFJ20
+ *
+ * File: parser.c
+ * Brief: Implementation of parser
+ *
+ * Authors: Hladký Tomáš    xhladk15@stud.fit.vutbr.cz
+ *          Kostolányi Adam xkosto04@stud.fit.vutbr.cz
+ *          Makiš Jozef     xmakis00@stud.fit.vutbr.cz
+ *          Bartko Jakub    xbartk07@stud.fit.vutbr.cz
+ */
+
+
+#include "parser.h"
+#include <stdio.h>
