f6c3c3d7bae78e2df0ee2a6cf4c6accd86154973
xbartk07 <xbartk07@stud.fit.vutbr.cz>
2020-10-26 20:03:42 +0100

    Added error() tests

--- tests/test_ll.c
+++ tests/test_ll.c
@@ -1,9 +1,26 @@
+/*
+ * Project: Compiler for imperative programing language IFJ20
+ *
+ * File: test_ll.c
+ * Brief: Tests of module ll.c
+ *
+ * Authors: Hladký Tomáš    xhladk15@stud.fit.vutbr.cz
+ *          Kostolányi Adam xkosto04@stud.fit.vutbr.cz
+ *          Makiš Jozef     xmakis00@stud.fit.vutbr.cz
+ *          Bartko Jakub    xbartk07@stud.fit.vutbr.cz
+ */
+
+
 #include <stdio.h>
 #include "../ll.h"
 
 int main() {
-	List * l = list_create();
-	list_destroy( l );
+	// Error tests (removed from final implementation)
+	// List * l_err = list_create();
+	Instruction * i_err = instr_create();
+
+	// List * l = list_create();
+	// list_destroy( l );
 
 	return 0;
 }
