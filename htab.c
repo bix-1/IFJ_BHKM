ca0e25bb6d2a94f771b01a1781081e2424f4e4fb
xbartk07 <xbartk07@stud.fit.vutbr.cz>
2020-10-26 01:20:21 +0100

    Added hash table files

---
+++ htab.c
@@ -0,0 +1,14 @@
+/*
+ * Project: Compiler for imperative programing language IFJ20
+ *
+ * File: htab.c
+ * Brief: Implementation of hash table functions
+ *
+ * Authors: Hladký Tomáš    xhladk15@stud.fit.vutbr.cz
+ *          Kostolányi Adam xkosto04@stud.fit.vutbr.cz
+ *          Makiš Jozef     xmakis00@stud.fit.vutbr.cz
+ *          Bartko Jakub    xbartk07@stud.fit.vutbr.cz
+ */
+
+#include "htab.h"
+#include <stdio.h>
---
+++ htab.h
@@ -0,0 +1,18 @@
+/*
+ * Project: Compiler for imperative programing language IFJ20
+ *
+ * File: htab.h
+ * Brief: Header file for htab.c
+ *
+ * Authors: Hladký Tomáš    xhladk15@stud.fit.vutbr.cz
+ *          Kostolányi Adam xkosto04@stud.fit.vutbr.cz
+ *          Makiš Jozef     xmakis00@stud.fit.vutbr.cz
+ *          Bartko Jakub    xbartk07@stud.fit.vutbr.cz
+ */
+
+ #ifndef HTAB_H
+ #define HTAB_H
+
+
+
+#endif
