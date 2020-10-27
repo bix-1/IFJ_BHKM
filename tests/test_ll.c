766d89c29a653340b8fad4fc6a4c0f8028b04b64
xbartk07 <xbartk07@stud.fit.vutbr.cz>
2020-10-27 21:31:20 +0100

    Added tests for instruction functions

--- tests/test_ll.c
+++ tests/test_ll.c
@@ -17,10 +17,47 @@
 int main() {
 	// Error tests (removed from final implementation)
 	// List * l_err = list_create();
-	// Instruction * i_err = instr_create();
+	// instr_t * i_err = instr_create();
 
 	List * l = list_create();
-	list_destroy( l );
+	printf( "\nSize of empty list: %d\n", list_size(l) );
+	printf( "Adding one instruction...\n" );
+	instr_t * i = instr_create();
+	instr_set_type( i, 1 );
+	list_add( l, i );
+	printf( "Size of list: %d\n\n", list_size(l) );
+
+	printf( "Adding 1st element...\n" );
+	instr_add_elem( i, "first_elem" );
+	printf( "The 1st elem now is: %s\n", instr_get_elem1(i) );
+	if ( instr_get_elem2(i) == NULL )
+		printf( "2nd element is empty...\n" );
+	printf( "Adding 2nd element...\n" );
+	instr_add_elem( i, "second_elem" );
+	if ( instr_get_elem1(i) != NULL && instr_get_elem2(i) != NULL )
+		printf(
+			"Elements of instruction now are: %s & %s\n\n",
+			instr_get_elem1(i), instr_get_elem2(i)
+		);
+
+	instr_t * j = instr_create();
+	instr_set_type( j, 2 );
+	instr_add_elem( j, "third_elem" );
+	instr_add_elem( j, "fourth_elem" );
+	list_add( l, j );
+	printf( "Contents of list:\n" );
+	int index = 0;
+	for ( instr_t * tmp = list_get_active(l); tmp != NULL; tmp = list_next(l) )
+	{
+		printf( "Instr %d\n", ++index );
+		printf( "\ttype: %d\n", instr_get_type(tmp) );
+		printf( "\telem1: %s\n", instr_get_elem1(tmp) );
+		printf( "\telem2: %s\n", instr_get_elem2(tmp) );
+	}
+
+	list_destroy( &l );
+	if ( l == NULL )
+		printf( "\nList destroyed\n\n" );
 
 	return 0;
 }
