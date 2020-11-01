598c88651facbf33a8775be3e1c48ca92c2b4f1e
Jozef Makis <xmakis00@stud.fit.vutbr.cz>
2020-11-01 16:12:56 +0100

    Added test file for scanner

---
+++ tests/scanner_tests/scanner_test.c
@@ -0,0 +1,18 @@
+/*
+ * Project: Compiler for imperative programing language IFJ20
+ *
+ * File: error.c
+ * Brief: Implementation of functions handling errors
+ *
+ * Authors: Hladký Tomáš    xhladk15@stud.fit.vutbr.cz
+ *          Kostolányi Adam xkosto04@stud.fit.vutbr.cz
+ *          Makiš Jozef     xmakis00@stud.fit.vutbr.cz
+ *          Bartko Jakub    xbartk07@stud.fit.vutbr.cz
+ */
+
+#include "scanner.h"
+
+int main() {
+
+}
+
