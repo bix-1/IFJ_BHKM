94b0f1c239d5a82d0cdf5a7e5a3129913c11c502
kostyio <akostolanyi@gmail.com>
2020-11-01 21:38:05 +0100

    Implementation of stack for expression processing

---
+++ stack.h
@@ -0,0 +1,29 @@
+/*
+ * Project: Compiler for imperative programing language IFJ20
+ *
+ * File: scanner.c
+ * Brief: Implementation of scanner.
+ *
+ * Authors: Hladký Tomáš    xhladk15@stud.fit.vutbr.cz
+ *          Kostolányi Adam xkosto04@stud.fit.vutbr.cz
+ *          Makiš Jozef     xmakis00@stud.fit.vutbr.cz
+ *          Bartko Jakub    xbartk07@stud.fit.vutbr.cz
+ */
+
+#include "scanner.h"
+
+
+
+typedef struct tokenStack
+{
+    Token top;
+    Token *nextTok;
+    
+
+}tokenStack;
+
+void stack_init(tokenStack *stack);
+void stack_push(tokenStack *stack, Token tokPush);
+void stack_pop(tokenStack *stack);
+void stack_empty(tokenStack *stack);
+void stack_free(tokenStack *stack);
