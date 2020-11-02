a273d1af97cf98eca2356aaa572895761c5cbc39
kostyio <akostolanyi@gmail.com>
2020-11-02 09:11:56 +0100

    Defined precTable & PT enums

--- expression.c
+++ expression.c
@@ -12,3 +12,40 @@
 
 #include <stack.h>
 #include <scanner.h>
+
+// Precedence table actions
+typedef enum
+{
+
+    S,  // Shift <
+    R,  // Reduce >
+    A,  // Accept
+    Eq, // Equal =
+    Err // Error
+
+} PT_action;
+
+// Precedence table tokens
+typedef enum
+{
+    OP_plus_minus,      // + -
+    OP_mult_div,        // * /
+    OP_parenth_open,    // (
+    OP_parenth_close,   // )
+    OP_rel_comp,        // < <= > >= == !=
+    OP_value,           // i
+    OP_dollar           // $
+
+} PT_operator;
+
+int precTable[7][7] = {
+//  
+    {R,S,S,R,R,S,R},
+    {R,R,S,R,R,S,R},
+    {S,S,S,Eq,S,S,Err},
+    {R,R,Err,R,R,Err,R},
+    {S,S,S,R,Err,S,R},
+    {R,R,Err,R,R,Err,R},
+    {S,S,S,Err,S,S,A},
+
+};
