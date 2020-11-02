9524eacafcb8a1c1deda520d9d9da3fe6c59b66f
kostyio <akostolanyi@gmail.com>
2020-11-02 08:37:49 +0100

    Implemented stack functions

--- stack.c
+++ stack.c
@@ -1,8 +1,8 @@
 /*
  * Project: Compiler for imperative programing language IFJ20
  *
- * File: scanner.c
- * Brief: Implementation of scanner.
+ * File: stack.c
+ * Brief: Implementation of token stack.
  *
  * Authors: Hladký Tomáš    xhladk15@stud.fit.vutbr.cz
  *          Kostolányi Adam xkosto04@stud.fit.vutbr.cz
@@ -12,22 +12,75 @@
 
 #include <stdlib.h>
 #include "stack.h"
-#include "scanner.h"
 
-// Initialization of the stack
 void stack_init(tokenStack *stack)
 {
-    stack->top.token_type = T_EMPTY;
-    stack->nextTok = NULL;
+    // Insterting the empty flag token
+    stackElemPtr newElem = (stackElemPtr)malloc(sizeof(stackElemPtr));
+
+    newElem->nextTok = NULL;
+    newElem->token.token_type = T_EMPTY;
+
+    stack->topToken = newElem;
 }
 
-void stack_push(tokenStack *stack, Token tokPush);
+void stack_push(tokenStack *stack, Token tokPush)
 {
-    Token *newToken = (Token *)malloc(sizeof(Token));
+    stackElemPtr newElem = (stackElemPtr)malloc(sizeof(stackElemPtr));
+
+    // Allocation error
+    if (!newElem)
+    {
+        // instert error call
+    }
+
+    newElem->token.token_type = tokPush.token_type;
+    newElem->token.attr = tokPush.attr;
+
+    // Push the token
+    newElem->nextTok = stack->topToken;
+    stack->topToken = newElem;
 }
 
+void stack_pop(tokenStack *stack)
+{
 
+    // If element on the top is not empty flag
+    if (stack->topToken->token.token_type != T_EMPTY)
+    {
+        stackElemPtr tmp = stack->topToken;
+        stack->topToken = stack->topToken->nextTok;
+        free(tmp);
+    }
+    else
+    {
+        //insert STACK_POP Error, trying to pop non-existent element
+    }
+}
 
-void stack_pop(tokenStack *stack);
-void stack_empty(tokenStack *stack);
-void stack_free(tokenStack *stack);
+void stack_empty(tokenStack *stack)
+{
+    // While only empty flag token is left on the stack
+    while (stack->topToken->token.token_type != T_EMPTY)
+    {
+        stack_pop(stack);
+    }
+}
+
+void stack_free(tokenStack *stack)
+{
+
+    if (stack->topToken == NULL)
+    {
+        return;
+    }
+
+    stackElemPtr tmp;
+
+    while (!stack->topToken)
+    {
+        tmp = stack->topToken;
+        stack->topToken = stack->topToken->nextTok;
+        free(tmp);
+    }
+}
