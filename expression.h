7d4694709529270fd2ddbe76a9a09303f121181b
kostyio <akostolanyi@gmail.com>
2020-11-02 08:31:54 +0100

    Initial commit

---
+++ expression.h
@@ -0,0 +1,14 @@
+/*
+ * Project: Compiler for imperative programing language IFJ20
+ *
+ * File: expression.h
+ * Brief: Expression proccesing header file
+ *
+ * Authors: Hladký Tomáš    xhladk15@stud.fit.vutbr.cz
+ *          Kostolányi Adam xkosto04@stud.fit.vutbr.cz
+ *          Makiš Jozef     xmakis00@stud.fit.vutbr.cz
+ *          Bartko Jakub    xbartk07@stud.fit.vutbr.cz
+ */
+
+#include "stack.h"
+#include "scanner.h"
