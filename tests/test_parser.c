f0281a5fc4dfcc84cba5706cd26f3edef0b254d7
xbartk07 <xbartk07@stud.fit.vutbr.cz>
2020-11-08 20:21:55 +0100

    Added new parser test

---
+++ tests/test_parser.c
@@ -0,0 +1,22 @@
+/*
+ * Project: Compiler for imperative programing language IFJ20
+ *
+ * File: parser.c
+ * Brief: Parser tests
+ *
+ * Authors: Hladký Tomáš    xhladk15@stud.fit.vutbr.cz
+ *          Kostolányi Adam xkosto04@stud.fit.vutbr.cz
+ *          Makiš Jozef     xmakis00@stud.fit.vutbr.cz
+ *          Bartko Jakub    xbartk07@stud.fit.vutbr.cz
+ */
+
+#include "../parser.h"
+#include <stdio.h>
+
+
+int main()
+{
+  parse();
+
+  return 0;
+}
