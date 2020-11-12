baa3c1ee297b3bf0388b74a31bf3348fb3f4a8b9
xbartk07 <xbartk07@stud.fit.vutbr.cz>
2020-11-12 21:05:17 +0100

    Fixed parser testing script

--- tests/parser_tests/test.sh
+++ tests/parser_tests/test.sh
@@ -16,7 +16,7 @@ echo "__________File in2__________"
 cat ${DIR}/in2
 echo "__________[TEST02]__________"
 echo
-./test_parser2 <${DIR}/in1
+./test_parser2 <${DIR}/in2
 
 echo
 echo
@@ -26,7 +26,7 @@ echo "__________File in3__________"
 cat ${DIR}/in3
 echo "__________[TEST03]__________"
 echo
-./test_parser2 <${DIR}/in1
+./test_parser2 <${DIR}/in3
 
 echo
 echo
@@ -36,6 +36,16 @@ echo "__________File in4__________"
 cat ${DIR}/in4
 echo "__________[TEST04]__________"
 echo
-./test_parser2 <${DIR}/in1
+./test_parser2 <${DIR}/in3
+
+echo
+echo
+echo "****************************"
+
+echo "__________File in5__________"
+cat ${DIR}/in5
+echo "__________[TEST05]__________"
+echo
+./test_parser2 <${DIR}/in5
 
 echo
