d91700eba4dafffde3bdf1194b9bea31bb61ef9a
xbartk07 <xbartk07@stud.fit.vutbr.cz>
2020-11-14 17:50:09 +0100

    Code refactoring

--- ll.c
+++ ll.c
@@ -17,12 +17,12 @@
 #include <stdlib.h>
 
 
-list_t * list_create ()
+list_t * list_create()
 {
   // create list
-  list_t * l = malloc( sizeof(list_t) );
-  if ( l == NULL )
-    error( 99, "ll.c", "list_create", "Failed to create list" );
+  list_t * l = malloc(sizeof(list_t));
+  if (l == NULL)
+    error(99, "ll.c", "list_create", "Failed to create instr_list");
   // init list
   l->first = NULL;
   l->active = NULL;
@@ -31,13 +31,13 @@ list_t * list_create ()
 }
 
 
-void list_destroy ( list_t ** l )
+void list_destroy (list_t ** l)
 {
-  if ( l == NULL || *l == NULL ) return;
+  if (l == NULL || *l == NULL) return;
 
   instr_t * curr = (*l)->first,
-              * next;
-  while ( curr != NULL )
+          * next;
+  while (curr != NULL)
   {
     // free instr
     next = curr->next;
@@ -45,19 +45,18 @@ void list_destroy ( list_t ** l )
     curr = next;
   }
 
-  free( *l );
+  free(*l);
   *l = NULL;
 }
 
 
-int list_size ( list_t * l )
+int list_size (list_t * l)
 {
-  if ( l == NULL ) return -1;
-  if ( l->first == NULL ) return 0;
+  if (l == NULL) return -1;
+  if (l->first == NULL) return 0;
   int n = 0;
   instr_t * tmp = l->first;
-  while ( tmp != NULL )
-  {
+  while (tmp != NULL) {
     tmp = tmp->next;
     n++;
   }
@@ -65,85 +64,84 @@ int list_size ( list_t * l )
 }
 
 
-void list_add ( list_t *l, instr_t * i )
+void list_add (list_t *l, instr_t * i)
 {
-  if ( l == NULL || i == NULL ) return;
+  if (l == NULL || i == NULL) return;
   // list empty
-  if ( l->first == NULL )
-  {
+  if (l->first == NULL) {
     l->first = i; l->active = i;
     return;
   }
   // get to last element
   instr_t * tmp = l->first;
-  while ( tmp->next != NULL )
+  while (tmp->next != NULL)
     tmp = tmp->next;
   // append to the end of list
   tmp->next = i;
 }
 
 
-instr_t * list_get_active ( list_t * l )
+instr_t * list_get_active (list_t * l)
 {
-  if ( l == NULL ) return NULL;
+  if (l == NULL) return NULL;
   return l->active;
 }
 
 
-instr_t * list_next ( list_t * l )
+instr_t * list_next (list_t * l)
 {
-  if ( l == NULL || l->active == NULL ) return NULL;
+  if (l == NULL || l->active == NULL) return NULL;
   l->active = l->active->next;
   return l->active;
 }
 
 
 
-instr_t * instr_create ()
+instr_t * instr_create()
 {
   // create instruction
-  instr_t * i = malloc( sizeof(instr_t) );
-  if ( i == NULL )
-    error( 99, "ll.c", "instr_create", "Failed to create instruction" );
+  instr_t * i = malloc(sizeof(instr_t));
+  if (i == NULL)
+    error(99, "ll.c", "instr_create", "Failed to create instruction");
   // initialize instruction
-  i->type   = -1;
-  i->elem1_key  = NULL;
-  i->elem2_key  = NULL;
-  i->next   = NULL;
+  i->type = -1;
+  i->elem1_key = NULL;
+  i->elem2_key = NULL;
+  i->next = NULL;
 
   return i;
 }
 
-void instr_set_type ( instr_t * i, int t )
+void instr_set_type (instr_t * i, int t)
 {
-  if ( i != NULL )  i->type = t;
+  if (i != NULL)  i->type = t;
 }
 
-int instr_get_type ( instr_t *i )
+int instr_get_type (instr_t *i)
 {
-  if ( i == NULL ) return -1;
+  if (i == NULL) return -1;
   return i->type;
 }
 
 
-void instr_add_elem ( instr_t * i, const char * e )
+void instr_add_elem (instr_t * i, const char * e)
 {
   // invalid or full
-  if ( i == NULL || i->elem2_key != NULL ) return;
-  if ( i->elem1_key == NULL ) // first free
+  if (i == NULL || i->elem2_key != NULL) return;
+  if (i->elem1_key == NULL) // first free
     i->elem1_key = (char *)e;
   else
     i->elem2_key = (char *)e;
 }
 
-const char * instr_get_elem1 ( instr_t * i )
+const char * instr_get_elem1 (instr_t * i)
 {
-  if ( i != NULL ) return i->elem1_key;
+  if (i != NULL) return i->elem1_key;
   return NULL;
 }
 
-const char * instr_get_elem2 ( instr_t * i )
+const char * instr_get_elem2 (instr_t * i)
 {
-  if ( i != NULL ) return i->elem2_key;
+  if (i != NULL) return i->elem2_key;
   return NULL;
 }
