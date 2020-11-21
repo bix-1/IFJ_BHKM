36f461c85e7895666c2697ab00187f55ca7a8a59
xbartk07 <xbartk07@stud.fit.vutbr.cz>
2020-11-22 00:35:25 +0100

    Updated cleanup method for IC_DEF_VAR

--- ll.c
+++ ll.c
@@ -43,8 +43,8 @@ void list_destroy (list_t ** l)
           * next;
   while (curr != NULL) {
     if (curr->type == IC_DEF_VAR) {
-      free(curr->elem_dest_ptr->symbol.sym_var_list);
-      free(curr->elem1_ptr->symbol.sym_var_list);
+      sym_var_list_free(curr->elem_dest_ptr->symbol.sym_var_list);
+      sym_var_list_free(curr->elem1_ptr->symbol.sym_var_list);
       free(curr->elem_dest_ptr);
       free(curr->elem1_ptr);
     }
