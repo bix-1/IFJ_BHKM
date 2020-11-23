052c2e418eda0263897057018912114aa2d599cb
xbartk07 <xbartk07@stud.fit.vutbr.cz>
2020-11-24 00:36:05 +0100

    Fixed id_find error call
    Segfault if used in expr_parser without updating the [next] token

--- parser.c
+++ parser.c
@@ -620,7 +620,7 @@ elem_t * id_find(scope_elem_t *scope, char *old_id) {
     // check for end of scope
     if (tmp_scope == NULL) {
       if (eps) return NULL;
-      else error(3, "parser", "match", "Variable \"%s\" undefined", to_string(&next));
+      else error(3, "parser", "match", "Variable \"%s\" undefined", old_id);
     }
     // get variable's contextual ID
     id = id_add_scope(tmp_scope, old_id);
